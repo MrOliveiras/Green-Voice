6fdb84d289d79fb573e2cdb46521112dce7975af
renatoalves2100 <renatoalves2100@outlook.com>
2019-12-04 10:18:07 -0300

    Cadastro de usuário (fim) - Renato

---
+++ app/src/main/java/com/example/greenvoice/Telas/Activity_contate.java
@@ -0,0 +1,14 @@
+package com.example.greenvoice.Telas;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.greenvoice.R;
+
+public class Activity_contate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle a) {
+        super.onCreate(a);
+        setContentView(R.layout.activity_contate);
+    }
+}
